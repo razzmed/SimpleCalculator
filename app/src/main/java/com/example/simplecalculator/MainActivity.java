51d4f8be3b43d18a9c993a1077648d3000a3f394
razzmed <mederrazzakov@gmail.com>
2020-04-02 19:03:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView result;
+    Double firstValues, secondValues, result_op;
+    String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = findViewById(R.id.result_field);
+    }
+
+    public void onNumberClick(View view) {
+        switch (view.getId()) {
+            case R.id.seven:
+                result.append("7");
+                break;
+            case R.id.eight:
+                result.append("8");
+                break;
+            case R.id.nine:
+                result.append("9");
+                break;
+            case R.id.four:
+                result.append("4");
+                break;
+            case R.id.five:
+                result.append("5");
+                break;
+            case R.id.six:
+                result.append("6");
+                break;
+            case R.id.one:
+                result.append("1");
+                break;
+            case R.id.two:
+                result.append("2");
+                break;
+            case R.id.three:
+                result.append("3");
+                break;
+            case R.id.zero:
+                result.append("0");
+                break;
+            case R.id.clear:
+                result.setText("");
+                break;
+            case R.id.dot:
+                String string = (result.getText().toString().trim());
+                if (string.length()>0){
+                    result.setText(string + ".");
+                    break;
+                }
+        }
+    }
+
+    public void onOperationClick(View view) {
+        switch (view.getId()) {
+            case R.id.plus:
+                firstValues = Double.valueOf(result.getText().toString());
+                result.setText(firstValues + "+");
+                operation = "+";
+                break;
+            case R.id.division:
+                firstValues = Double.valueOf(result.getText().toString());
+                result.setText(firstValues + "/");
+                operation = "/";
+                break;
+            case R.id.minus:
+                firstValues = Double.valueOf(result.getText().toString());
+                result.setText(firstValues + "-");
+                operation = "-";
+                break;
+            case R.id.multiplication:
+                firstValues = Double.valueOf(result.getText().toString());
+                result.setText(firstValues + "*");
+                operation = "*";
+                break;
+            case R.id.equally:
+                if (operation != null) {
+                    String two = result.getText().toString().replace(firstValues.toString() + operation, "");
+                    secondValues = Double.valueOf(two);
+                    switch (operation) {
+                        case "+":
+                            plusOperation();
+                            break;
+                        case "/":
+                            divisionOperation();
+                            break;
+                        case "-":
+                            minusOperation();
+                            break;
+                        case "*":
+                            multiplicationOperation();
+                            break;
+                    }
+                }
+
+        }
+    }
+
+    public void plusOperation() {
+        result_op = firstValues + secondValues;
+        result.setText(result_op.toString());
+    }
+
+    public void divisionOperation() {
+        result_op = firstValues / secondValues;
+        result.setText(result_op.toString());
+
+    }
+
+    public void minusOperation() {
+        result_op = firstValues - secondValues;
+        result.setText(result_op.toString());
+    }
+
+    public void multiplicationOperation() {
+        result_op = firstValues * secondValues;
+        result.setText(result_op.toString());
+    }
+}
